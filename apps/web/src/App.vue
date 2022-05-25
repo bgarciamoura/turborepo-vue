41f7a64288b1c94f8d2b394695fac0f530d7aeba
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-05-25 16:07:44 -0300

    Very first commit

---
+++ apps/web/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld
+    msg="Hello Vue 3 + TypeScript + Vite inside a beautiful monorepo using Turborepo"
+  />
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+defineComponent({
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
